da274c1ea7197f9f32069df22128e9106dcd74ec
Anton <kibekin1984anton@gmail.com>
2022-04-28 12:52:06 +0300

    test middleware

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
+//Обработка ошибок, последний Middleware
+app.use(errorHandler)
+
 const start = async () => {
     try {
         await sequelize.authenticate()
