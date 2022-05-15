38eca72f9377350d3974cfe5c05f0a43b61aa85e
Anton <kibekin1984anton@gmail.com>
2022-05-15 10:15:03 +0300

    implementation dropdown element

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Modal, Button, Form, FormControl } from 'react-bootstrap'
+import React, { useContext, useState } from 'react';
+import { Modal, Button, Form, Dropdown, FormControl } from 'react-bootstrap'
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import { Context } from '../..';
 
-const CreateDevice = ({show, onHide}) => {
+const CreateDevice = ({ show, onHide }) => {
+    const { device } = useContext(Context)
+    const [info, setInfo] = useState([])
+
     return (
         <Modal
             show={show}
@@ -16,9 +23,37 @@ const CreateDevice = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
+                    <Dropdown className='mt-2 mb-2'>
+                        <DropdownToggle>Выберите тип</DropdownToggle>
+                        <DropdownMenu>
+                            {device.types.map(type =>
+                                <DropdownItem key={type.id}>{type.name}</DropdownItem>
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                    <Dropdown className='mt-2 mb-2'>
+                        <DropdownToggle>Выберите брэнд</DropdownToggle>
+                        <DropdownMenu>
+                            {device.brands.map(brand =>
+                                <DropdownItem key={brand.id}>{brand.name}</DropdownItem>
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
                     <FormControl
-                        placeholder={'Введите название типа'}
+                        className='mt-3'
+                        placeholder='Введите название устройства'
                     />
+                    <FormControl
+                        className='mt-3'
+                        placeholder='Введите стоимость устройства'
+                        type='number'
+                    />
+                    <FormControl
+                        className='mt-3'
+                        type='file'
+                    />
+                    <hr/>
+                    
                 </Form>
             </Modal.Body>
             <Modal.Footer>
